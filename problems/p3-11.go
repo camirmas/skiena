d26270c8c624c49b86df6c792a92d29403beeaec
Cam <cirmas@protonmail.com>
2019-02-07 16:27:05 -0800

    Add problem 3-11

--- problems/p3-11.go
+++ problems/p3-11.go
@@ -33,3 +33,94 @@ func min(list []int) int {
 
 	return min
 }
+
+type TreeC3p11b struct {
+	root *Node
+	list []int
+}
+
+type Node struct {
+	Key    int
+	Val    int
+	Left   *Node
+	Right  *Node
+	Parent *Node
+}
+
+func NewTreeC3p11b(list []int) TreeC3p11b {
+	t := TreeC3p11b{nil, list}
+
+	for idx, key := range list {
+		t.Insert(key, idx)
+	}
+
+	return t
+}
+
+func (t *TreeC3p11b) Insert(key, val int) {
+	if t == nil {
+		return
+	}
+	if t.root == nil {
+		t.root = &Node{key, val, nil, nil, nil}
+	} else {
+		t.root.insert(key, val)
+	}
+}
+
+func (node *Node) insert(key, val int) {
+	if node == nil {
+		return
+	}
+	if key > node.Key {
+		if node.Right == nil {
+			node.Right = &Node{key, val, nil, nil, node}
+		} else {
+			node.Right.insert(key, val)
+		}
+	} else {
+		if node.Left == nil {
+			node.Left = &Node{key, val, nil, nil, node}
+		} else {
+			node.Left.insert(key, val)
+		}
+	}
+}
+
+func (t *TreeC3p11b) Search(key int) *Node {
+	if t == nil || t.root == nil {
+		return nil
+	}
+	return t.root.search(key)
+}
+
+func (node *Node) search(key int) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if node.Key == key {
+		return node
+	}
+	if key < node.Key {
+		return node.Left.search(key)
+	} else {
+		return node.Right.search(key)
+	}
+}
+
+func (t *TreeC3p11b) Min(i, j int) int {
+	iNode := t.Search(t.list[i])
+
+	min := iNode
+	current := iNode
+
+	for current != nil {
+		if current.Left != nil && current.Val <= j {
+			min = current.Left
+		}
+		current = current.Left
+	}
+
+	return min.Key
+}
--- problems/p3-11_test.go
+++ problems/p3-11_test.go
@@ -15,5 +15,23 @@ func TestC3p11a(t *testing.T) {
 }
 
 func TestC3p11b(t *testing.T) {
+	d := NewTreeC3p11b([]int{1, 2, 3})
 
+	if min := d.Min(0, 2); min != 1 {
+		t.Errorf("Expected 1, got %d", min)
+	}
+
+	if min := d.Min(1, 2); min != 2 {
+		t.Errorf("Expected 2, got %d", min)
+	}
+
+	d = NewTreeC3p11b([]int{1, 3, 2})
+
+	if min := d.Min(0, 2); min != 1 {
+		t.Errorf("Expected 1, got %d", min)
+	}
+
+	if min := d.Min(1, 2); min != 2 {
+		t.Errorf("Expected 2, got %d", min)
+	}
 }
